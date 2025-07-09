9d1e147445b36960753546015513c8914e30f51d
latheriault <lisatheriault5@gmail.com>
2025-07-09 10:08:07 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'anxietySOS-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
